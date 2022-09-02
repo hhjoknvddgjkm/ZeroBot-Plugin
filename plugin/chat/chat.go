0d61c7e05d8858071555f7f6f9eb7791a3642b0b
hhjoknvddgjkm <85065657+hhjoknvddgjkm@users.noreply.github.com>
2022-09-03 01:58:26 +0800

    Update chat.go

--- plugin/chat/chat.go
+++ plugin/chat/chat.go
@@ -45,18 +45,22 @@ func init() { // 插件主体
 				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("请不要戳", nickname, " >_<"))
-			case poke.Load(ctx.Event.GroupID).Acquire():
-				// 5分钟共8块命令牌 一次消耗1块命令牌
+			case poke.Load(ctx.Event.GroupID).Acquire(3):
+				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("喂(#`O′) 戳", nickname, "的坤巴", "干嘛！"))
-			case poke.Load(ctx.Event.GroupID).Acquire():
-				// 5分钟共8块命令牌 一次消耗1块命令牌
+			case poke.Load(ctx.Event.GroupID).Acquire(3):
+				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("再戳", ",我就测你们码"))
-			case poke.Load(ctx.Event.GroupID).Acquire():
-				// 5分钟共8块命令牌 一次消耗1块命令牌
+			case poke.Load(ctx.Event.GroupID).Acquire(3):
+				// 5分钟共8块命令牌 一次消耗3块命令牌
 				time.Sleep(time.Second * 1)
 				ctx.SendChain(message.Text("别戳了", ",再戳老子坤巴要射了！！！"))
+			case poke.Load(ctx.Event.GroupID).Acquire(3):
+				// 5分钟共8块命令牌 一次消耗3块命令牌
+				time.Sleep(time.Second * 1)
+				ctx.SendChain(message.Text("呜呜呜", ",你的丁真哥哥不想破处"))
 			default:
 				// 频繁触发，不回复
 			}
